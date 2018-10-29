6e1952544f5f7647d95b2c0dfc9dda88d5cb0fdd
tianYu <tianyu@gouwangminhaodeMacBook-Pro.local>
2018-10-29 10:43:03 +0800

    读取文件行数实现

--- src/dirSc.py
+++ src/dirSc.py
@@ -1,1 +1,14 @@
 #coding=utf-8
+
+import os
+
+
+file0=r'/Users/tianyu/iotdb-benchmark/src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/opentsdb/OpenTSDB.java'
+
+#读取文件的行数
+def count_fileR(file):
+    with open(file,'r',encoding='utf-8') as f:
+        num=len(f.readlines());
+    return num
+
+print(count_fileR(file0))
